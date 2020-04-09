876494a8dc29c79ca407311419382188acea781c
tarek008 <59961894+tarek008@users.noreply.github.com>
2020-04-09 10:10:31 -0500

    Update joeur.h

--- joeur.h
+++ joeur.h
@@ -8,14 +8,24 @@
 #include <SDL/SDL_mixer.h>
 #include <string.h>
 #include <math.h>
-
-
+ #define FPS 60
 #define TEMPS 30
-#define FPS 60
+
 
 //****** HEAD STRUCTURE *******
 
-
+typedef struct BACKGROUND
+{
+SDL_Surface*  bg;
+SDL_Surface* bg_collision;
+SDL_Rect position;
+SDL_Rect position_collision;
+//SDL_Rect scroll;
+int stage;
+//int speed;
+    SDL_Rect cam;
+SDL_Surface* fenetre;
+}BACKGROUND;
 
 typedef struct PERSO_PRINCIPALE
 {
@@ -29,26 +39,19 @@ typedef struct PERSO_PRINCIPALE
 	SDL_Rect pos_score;
         int stage; 
 }PERSO_PRINCIPALE;
-
-
-
-
-
 //****** HEAD PERSONAGE *******
 
 
 void init_persoP(PERSO_PRINCIPALE *p);
 void afficher_personnageP(PERSO_PRINCIPALE *perso,SDL_Surface *fenetre);
-void deplacement_clavier(PERSO_PRINCIPALE *perso,int sens,int bg , int vitesse);
+void deplacement_clavier(PERSO_PRINCIPALE *perso,int sens,BACKGROUND *bg, int vitesse);
 void animer_clavier(PERSO_PRINCIPALE * perso,int sens);
 
+//********* HEAD BACKGROUD *******
 
-
-
-
-
-
-
+void init_bk(BACKGROUND* b);
+void afficher_bk(BACKGROUND *bk,SDL_Surface *fenetre);
+void scrollingbg(int sens,BACKGROUND *bg,PERSO_PRINCIPALE *p,double xVel);
 
 
 #endif
