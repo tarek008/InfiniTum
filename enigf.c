e9fa739d13abb89fb5baf6cc6ca2dcc0b3489dbe
medhama <60546106+medhama@users.noreply.github.com>
2020-06-18 20:09:43 +0100

    Add files via upload

--- enigf.c
+++ enigf.c
@@ -1,29 +1,29 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
-#include<time.h>
+#include <time.h>
 #include <SDL/SDL.h>
 #include <SDL/SDL_mixer.h>
 #include <SDL/SDL_image.h>
-#include "enig.h"
+#include "joeur.h"
+#include <math.h>
+#include <tgmath.h>
 
 void init_enigme(enigme * e)//
 {
 	e->p.x=0;
 	e->p.y=0;
 	e->img=NULL;
-
-
 }
 
- void generate_afficher (SDL_Surface * screen  , char image [],enigme *e,int *alea)
+void generate_afficher (SDL_Surface * screen  , char image [],enigme *e,int *alea)
 {
-	int test=*alea ;
-    srand(time(NULL));
+int test=*alea ;
+srand(time(NULL));
 do{
- *alea = 1+ rand()%8;
+ *alea = 1+ rand()%3;
 }while(*alea==test) ;
- sprintf(image ,"%d.png",*alea);
+ sprintf(image ,"fathallah/%d.png",*alea);
 e->img = IMG_Load(image);
  SDL_BlitSurface(e->img,NULL,screen,&(e->p)) ;
   SDL_Flip(screen) ;
@@ -31,22 +31,22 @@ e->img = IMG_Load(image);
 
  int solution_e (char image [])
  {
- 	int solution =0 ;
+ 	int solution =0;
 
- 	if(strcmp(image,"1.png")==0)
+ 	if(strcmp(image,"fathallah/1.png")==0)
  	{
      		solution =2 ;
  	}
- 	else  	if(strcmp(image,"2.png")==0)
- 	{
- 		solution =2;
-
- 	}
- 	else 	if(strcmp(image,"3.png")==0)
+ 	else  if(strcmp(image,"fathallah/2.png")==0)
  	{
  		solution =1;
+
  	}
-	else  	if(strcmp(image,"4.png")==0)
+ 	 else if(strcmp(image,"fathallah/3.png")==0)
+ 	{
+ 		solution =3;
+ 	}
+	/*else  	if(strcmp(image,"4.png")==0)
  	{
  		solution =1;
 
@@ -69,7 +69,7 @@ e->img = IMG_Load(image);
  	{
  		solution =2;
 
- 	}
+ 	} */
 
 
  	return solution;
@@ -78,7 +78,7 @@ e->img = IMG_Load(image);
 int resolution (int * running,int *run )//
 {
 	SDL_Event event ;
-  int r=0 ;
+  		int r=0 ;
 	SDL_PollEvent(&event);
 	switch(event.type)
 	{
@@ -100,10 +100,8 @@ int resolution (int * running,int *run )//
 			   r=3 ;
 			   break;
 
-			    }
+		}
        break ;
-
-
 	}
   return r ;
 }
@@ -113,15 +111,146 @@ int resolution (int * running,int *run )//
 
  	if (r==s)
  	{
- 		en->img=IMG_Load("11.png");
+ 		en->img=IMG_Load("fathallah/11.png");
  		SDL_BlitSurface(en->img, NULL, screen, &(en->p)) ;
         SDL_Flip(screen);
  	}
  	else
  	{
- 		en->img=IMG_Load("00.png");
+ 		en->img=IMG_Load("fathallah/00.png");
  		SDL_BlitSurface(en->img, NULL, screen, &(en->p)) ;
         SDL_Flip(screen);
  	}
+
  }
+
+
+
+int randGenerFunc(SDL_Surface *screen, float optt[],int *aa,int *bb)
+{
+    int ran,ranPM,j=0;
+    float c,op[3],ranNum;
+    SDL_Surface *texte[3]={NULL};
+    SDL_Rect positionTexte[3]={0};
+    TTF_Font *police = NULL;
+    SDL_Color couleurNoire = {0, 0, 0};
+    police = TTF_OpenFont("font.ttf", 30);
+    srand(time(0));
+
+    (*aa) = rand()%(20)+1;
+    (*bb) = rand()%(20)+1;
+	
+    c=sqrt((*aa)*(*aa)+(*bb)*(*bb));
+    printf("\n a=%d",(*aa));
+    printf("\n b=%d",(*bb));
+    printf("\n %f",c);
+    ran = rand()%(3-1+1)+1;
+
+
+
+char affiche[10][10];
+    for(int i=0;i<3;i++)
+    {
+       // srand(time(0));
+        if(ran==i+1)
+        {
+            printf("\n opTT %d: %.2f",i+1,c);
+	    sprintf(affiche[i],"%.2f",c);
+	    optt[i]=c;
+        }
+        else
+        {
+            float r = (float)rand()/(float)RAND_MAX;
+            ranNum =(float)(rand()%(5-1+1)+1+r);
+            printf("\n ranNUM");
+            printf("\n %f",ranNum);
+            ranPM =rand()%(2);
+            printf("\n ranPM");
+            printf("\n %d",ranPM);
+            if(ranPM==1)
+            {
+                op[j]=fabs(c-ranNum);
+                printf("\n  opNEG: %.2f",op[j]);
+		sprintf(affiche[i],"%.2f",c);
+		optt[i]=op[j];
+                j++;
+            }
+            else
+            {
+                op[j]=c+ranNum;
+                printf("\n opPOS: %.2f",op[j]);
+		sprintf(affiche[i],"%.2f",c);
+		optt[i]=op[j];
+                j++;
+            }
+        }
+    }
+    //printf("\n Given two sides of a right triangle calculate the remaining side");
+    return ran;
+}
+
+void blitTXT(SDL_Surface * screen, float optt[],int *aa,int *bb)
+{
+
+SDL_Surface *img;
+SDL_Rect p;
+p.x=0;
+p.y=0;
+
+
+
+	 SDL_Surface *texte[3]={NULL},*ab[2]={NULL};
+    SDL_Rect positionTexte[3]={0},posab[2]={0};
+    TTF_Font *police = NULL;
+    SDL_Color couleurNoire = {0, 0, 0};
+    police = TTF_OpenFont("font.ttf", 30);
+	//printf("\n partie1");
+
+img = IMG_Load("fathallah/engimealea1.png");
+ SDL_BlitSurface(img,NULL,screen,&p) ;
+char aa1[10],bb1[10];
+char affiche[10][10];
+sprintf(aa1,"a=%d",(*aa));
+sprintf(bb1,"b=%d",(*bb));
+
+sprintf(affiche[0],"%.2f",optt[0]);
+sprintf(affiche[1],"%.2f",optt[1]);
+sprintf(affiche[2],"%.2f",optt[2]);
+
+
+ab[0]=TTF_RenderText_Blended(police,aa1, couleurNoire);
+ab[1]=TTF_RenderText_Blended(police,bb1, couleurNoire);
+
+posab[0].x =270;
+        posab[0].y = 275;
+        SDL_BlitSurface(ab[0], NULL, screen, &posab[0]); /* Blit du texte */
+posab[1].x =440;
+        posab[1].y = 275;
+        SDL_BlitSurface(ab[1], NULL, screen, &posab[1]); /* Blit du texte */
+	
+ //printf("\n partie2");
+texte[0] = TTF_RenderText_Blended(police,affiche[0], couleurNoire);
+	texte[1] = TTF_RenderText_Blended(police, affiche[1], couleurNoire);
+	texte[2] = TTF_RenderText_Blended(police, affiche[2], couleurNoire);
+
+//printf("\n partie3");
+positionTexte[0].x =270;
+        positionTexte[0].y = 440;
+        SDL_BlitSurface(texte[0], NULL, screen, &positionTexte[0]); /* Blit du texte */
+
+positionTexte[1].x =450;
+        positionTexte[1].y = 440;
+        SDL_BlitSurface(texte[1], NULL, screen, &positionTexte[1]); 
+
+positionTexte[2].x =630;
+        positionTexte[2].y = 440;
+        SDL_BlitSurface(texte[2], NULL, screen, &positionTexte[2]); 
+	
+	
+	SDL_Flip(screen);
+
+}
+
+
+
 
--- main.c
+++ main.c
@@ -1,15 +1,93 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<string.h>
-#include <SDL/SDL.h>
+#include "SDL/SDL.h"
+#include <stdio.h>
+#include <stdlib.h>
 #include <SDL/SDL_mixer.h>
 #include <SDL/SDL_image.h>
-#include "enig.h"
-//#include "enigf.c"
-int main()
-{ 
+#include "joeur.h"
+#include "fire.h"
+#include <math.h>
+#include <tgmath.h>
 
-	SDL_Surface *screen,*image1 ;
+void main(SDL_Surface* screen){
+
+int x=3;
+int xscore=0;
+int fgh=0;
+int pos_perso=500;
+int r2=0;
+//The images
+PERSO_PRINCIPALE perso;
+	BACKGROUND b;
+ENTITE_SECONDAIRE ennemi;
+int dT;
+   double FrmStrt=0,FrmEnd=0;
+
+	SDL_Event event;
+	int sens;
+int direction=0;
+		SDL_Rect posdT;
+   double xVel,yVel,canJump=1;
+   double v_jump = -7;
+   double gravity = 0.2;
+   int right,left;
+
+
+//collision moraba3
+//pos2.x =700;
+//pos2.y = 280;
+
+pos2.x =700;
+pos2.y = 250;
+
+
+/*rect = SDL_CreateRGBSurface(0,32,32,32,0,0,0,0);
+rectperso = SDL_CreateRGBSurface(0,32,70,32,0,0,0,0);*/
+
+
+//SDL_FillRect(rectperso,NULL,0xffffff);
+//SDL_FillRect(rect,NULL,0xffffff);
+
+rect =IMG_Load("1.png");
+
+
+SDL_Init(SDL_INIT_EVERYTHING);
+	TTF_Init();
+	Time time;
+	initializerTemps(&time);
+
+
+
+
+
+Score score;
+	initialiserScore(&score);
+Vie vie;
+initialiserVie(&vie);
+	char string[20];
+
+SDL_Rect posScore;
+posScore.x=490;
+	posScore.y=350;
+
+
+	//initialiser la fenetre principal
+	b.fenetre=screen;
+	b.stage=1;
+	//init bk & perso
+	init_persoP(&perso);
+	init_bk(&b);
+ 
+//initi fire 
+Clochard c;
+	CLOCHARD_Init(&c);
+
+
+
+
+
+
+//enigme
+SDL_Surface *image1 ;
 	SDL_Rect p ;
 	p.x=0 ;
 	p.y=0 ;
@@ -17,87 +95,374 @@ int main()
 	FILE *f ;
 	int s,r,run =1,running=1,alea;
 	char image[30]="";
-	 SDL_Event event;
    f=fopen("test.txt","a") ;
-	 
-	 SDL_Init ( SDL_INIT_VIDEO ) ;
 
-	 screen=SDL_SetVideoMode(1366,768,32,SDL_HWSURFACE  |  SDL_DOUBLEBUF | SDL_FULLSCREEN );
-
-
-	 init_enigme(&e);
-	
-	 if(e.img==NULL)
+//init enigme
+ init_enigme(&e);
+ if(e.img==NULL)
 	 {
 	 	fprintf(f,"%d\n",1800) ;
-
 	 }	
     
-	
-	 	 
-	 
-	 while (run)
-	 {
-		 running=1,r=0 ;
-	   SDL_PollEvent(&event);
-        switch(event.type)
-        {
-            case SDL_QUIT:
-                run = 0;
-	 break ;
+
+ //déclaration entite secondaire
+        Entite E[8] ;
+        int y = 0;
+        int posmax = 400;
+        int posmin = 250;
+        int posmax_x = 600;
+        int posmin_x = 200;
+ SDL_Rect positionecran;
+initEntite(E);
+
+
+
+
+
+
+//img gameover
+SDL_Surface *gameover;
+gameover=IMG_Load("gameoverr.png");
+
+
+
+
+
+
+
+//Start SDL
+    if(SDL_Init( SDL_INIT_VIDEO )!=0)
+      {
+        printf("Unable to initialize SDL: %s\n",SDL_GetError());
+      };
+//Set up screen
+    screen = SDL_SetVideoMode(1200, 520, 32, SDL_HWSURFACE);
+     if(screen == NULL)
+      {
+        printf("Unable to set video mode: %s",SDL_GetError());
+      }
+
+
+
+
+int continuer=1;
+int vitesse=0;
+int sprint=0;
+SDL_EnableKeyRepeat(100,100);
+
+
+
+
+//jump
+   void jump()
+	{
+	if (canJump==1) yVel=v_jump;
+	}
+
+
+
+while(continuer){
+		FrmEnd=SDL_GetTicks();
+	  	afficher_bk(&b,screen);
+nbVie(0,0,&vie,screen);
+//entite secondaire
+     blitEntite(E,screen);
+     deplacerEntite(E,screen,positionecran,posmax,posmin,posmax_x,posmin_x);
+
+
 	
 
-        }	
-        	fprintf(f,"%d\n",e.p.x) ;
-	         fprintf(f,"%d\n",e.p.y) ;
-         generate_afficher ( screen  , image ,&e,&alea) ;
-	        fprintf(f,"%s\n",image) ;
+    while(SDL_PollEvent(&event))
+		{
+			switch(event.type)
+			{
+				case SDL_QUIT:
+				continuer=0;
+				break;
 
-      s=solution_e (image );
-			do{
-			r=resolution (&running,&run);
-			}while((r>3 || r<1) && running!=0) ;
-			fprintf(f,"run=%d\n",run) ;
-			fprintf(f,"s= %d\nr=%d\n",s,r) ;
-			
-      while(running){
+				case SDL_KEYDOWN:
+				switch (event.key.keysym.sym)
+				{
+					case SDLK_ESCAPE:
+					continuer = 0;
+					break;
 
-				afficher_resultat (screen,s,r,&e) ;
-			  SDL_WaitEvent(&event);
-        switch(event.type)
-        {
-					 case SDL_QUIT :
-            running =0 ;
-						run=0 ;
-					  break ;
-            case SDL_KEYDOWN :
-						    
-                  switch( event.key.keysym.sym )
-                {
-			             case  SDLK_ESCAPE: 
-			              running= 0 ;
-run=0 ;
 
-			              break ;
-case SDLK_SPACE:
-running=0;
-break;
-			  
-			   
-			          }
-						break ;
-        }
-				
-			
+                               case SDLK_s:
+                               sprint =1;
+                                     break;
+				case SDLK_m:
+				do
+{
+					r2=mapaffiche(screen,pos_perso);
+fgh=0;
+}while(r2!=1);
+				break; 
 
-			}
-			
-	
-	
-	
-   }
-	 fclose(f) ;
-      SDL_FreeSurface(screen);
-      SDL_Quit();
-	return 0;
+                          case SDLK_u: 
+		            SaveGame("save.txt",perso,b,vie);
+
+		            break;
+		            case SDLK_l: 
+		            LoadGame("save.txt",&perso,&b,&vie);
+                            x=vie.valVie;
+                            //xscore=
+		            break;
+
+
+		         	case SDLK_RIGHT: // Flèche droite
+	           		sens=1;
+                                 right=1;		   
+		           	//animer_clavier(&perso, sens);
+		           	//deplacement_clavier(&perso,sens,&b,vitesse);
+		           	break;
+
+                                        case SDLK_SPACE:
+                                          jump();
+                                        break;
+
+
+
+		            case SDLK_LEFT: // Flèche gauche
+		            sens=2;
+                                 left=1;
+		           // animer_clavier(&perso, sens) ;
+		            //deplacement_clavier(&perso,sens,&b,vitesse);
+		            break;
+
+		        }
+		        break;
+
+
+
+
+case SDL_MOUSEBUTTONDOWN:
+if(SDL_BUTTON_LEFT)
+{
+int k=event.motion.x;
+deplacement_sourie(&perso,&sens,k,&right,&left);
+
 }
+break;
+
+
+case SDL_MOUSEBUTTONUP :
+{(sens)=0;
+right=0; left=0;
+break;
+}
+
+
+
+
+		        case SDL_KEYUP:
+				switch (event.key.keysym.sym)
+				{
+					case SDLK_ESCAPE:
+					continuer = 0;
+					break;
+
+
+
+                                case SDLK_s: 
+                                       sprint =0;
+		           	break;
+
+		         	case SDLK_RIGHT: 
+                                  right=0;
+		           	break;
+
+		            case SDLK_LEFT: 
+                                  left=0;
+		            break;
+
+		        }
+		        break;
+			
+		    }
+
+    }	    
+
+
+
+	//x movement
+	if(right==1) {
+xVel+=0.3;
+animer_clavier(&perso,sens);
+
+}
+
+
+	if(left==1) {
+xVel-=0.3;
+animer_clavier(&perso,sens);
+
+}
+
+
+	if(right ==0 && xVel>0.2) xVel-=0.1;
+	if(left ==0 && xVel<-0.2) xVel+=0.1;
+
+	if(right==0 && left ==0 && xVel>=-0.2 && xVel<0.2) xVel=0; //STOP threshhold
+
+
+//speed controle
+if(sprint==1)
+{
+vitesse=9;
+}
+else{
+vitesse=5;
+}
+
+
+perso.position.x +=xVel;
+pos_perso +=xVel;
+//printf("\n persoo: %d",pos_perso);
+//printf("\n xVel: %f",xVel);
+ perso.position.y +=yVel;
+yVel += gravity*2;
+
+if (perso.position.y > 200){
+	yVel=0;canJump=1;}
+if(perso.position.y < 200){
+ canJump=0;
+}
+
+//enigme
+if(perso.position.x >= 700){
+
+ while(running){
+int yui=1;
+float optt[3];
+if(yui==1)
+{
+	int aa,bb;
+	s=randGenerFunc(screen,optt,&aa,&bb);
+	//printf("\n aaaa \n aaaa %f",optt[0]);
+//	printf("\n aaaa \n aaaa %f",optt[1]);
+//	printf("\n aaaa \n aaaaaaaaaaaaaaaaa %f",optt[2]);
+	blitTXT(screen,optt,&aa,&bb);
+	do{
+	
+	r=resolution (&running,&run);
+	}while((r>3 || r<1) && running!=0) ;
+
+}
+else
+{
+ generate_afficher (screen, image, &e,&alea) ;
+ s=solution_e (image);
+
+do{
+
+r=resolution (&running,&run);
+}while((r>3 || r<1) && running!=0) ;
+}
+afficher_resultat (screen,s,r,&e) ;
+running=0;
+
+if(r==s && x<3){
+x++;
+vie.valVie=x;
+}
+
+if(r==s){
+xscore+=1000;
+score.scoreActuel=xscore;
+}
+
+}
+
+if(r!=s){
+x--;
+perso.position.x=0;
+vie.valVie=x;
+running=1;
+}
+
+}
+
+
+if(xVel>vitesse) xVel=vitesse;
+if(xVel<-vitesse) xVel=-vitesse;
+
+
+
+
+
+
+ afficher_personnageP(&perso,screen);
+//SDL_BlitSurface(rectperso,NULL,screen,&perso.position);
+
+
+scrollingbg(sens,&b,&perso,xVel);
+
+
+
+
+
+afficherTemps(&time,&screen);
+			SDL_Delay(12);
+
+
+
+
+afficherScore(screen,&score,b,0);
+sprintf(string, "score = %d ", score.scoreActuel);
+
+
+CLOCHARD_Render(&c,&screen);
+
+
+
+//collision moraba3
+/*if(collision(rectperso,perso.position,rect,pos2)){
+				perso.position.x=0;
+x--;
+score.scoreActuel=xscore;
+vie.valVie=x;
+		}*/
+
+/*if(collision(perso.position,pos2)){
+				perso.position.x=0;
+x--;
+vie.valVie=x;
+		}*/
+
+
+
+if(vie.valVie==0){
+SDL_BlitSurface(gameover, NULL, screen,0);
+}
+
+//SDL_BlitSurface(rect,NULL,b.bg,&pos2);
+//CLOCHARD_Render(&c,&b.bg);
+miniMapCorner(screen,pos_perso,perso.position.y);
+
+
+
+
+
+SDL_Flip(screen);
+
+dT=SDL_GetTicks()-FrmEnd;
+	if (1000/FPS>dT)
+	{
+        SDL_Delay(1000/FPS-dT);
+	}
+
+
+}
+
+
+SDL_EnableKeyRepeat(0,0);
+
+    SDL_FreeSurface(screen);    
+
+
+  //  pause=getchar();
+
+}
+
+
